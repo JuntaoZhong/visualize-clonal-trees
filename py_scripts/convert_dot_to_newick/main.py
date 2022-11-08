6e8e553e71d1bbdb8e561b742e8de006297f31fc
root <root@LAPTOP-GDKPC3CG.localdomain>
2022-11-07 19:04:01 -0600

    distance measures code

---
+++ py_scripts/convert_dot_to_newick/main.py
@@ -0,0 +1,29 @@
+import newick
+import pydot
+import networkx
+
+def convert_dot_to_newick(filename):
+  graphs = pydot.graph_from_dot_file(filename)
+  graph = graphs[0]
+  graph = networkx.drawing.nx_pydot.from_pydot(graph)
+  # Get all the nodes.
+  graph_nodes = [node for node in graph.nodes if "label" in graph.nodes[node]] 
+  nodes = []
+  nodes_dict = {} 
+  for node in graph_nodes:
+    newick_node = newick.Node(graph.nodes[node]["label"].replace("\"", ""))
+    nodes.append(newick_node)
+    nodes_dict[node] = newick_node 
+  print(nodes_dict)
+  for edge in graph.edges:
+    parent = f"{edge[0]}"  
+    child = f"{edge[1]}"  
+    parent_node = nodes_dict[parent] 
+    child_node = nodes_dict[child] 
+    parent_node.add_descendant(child_node)
+  print(newick.dumps(nodes_dict["a"]))
+     
+    
+convert_dot_to_newick("../../examples/trees/tree1.dot")
+
+
---
+++ py_scripts/parent-child-dot.py
@@ -0,0 +1,32 @@
+import pydot
+import networkx
+
+def read_dot_file(filename):
+  graphs = pydot.graph_from_dot_file(filename)
+  graph = graphs[0]
+  tree = networkx.drawing.nx_pydot.from_pydot(graph)
+  print(tree.pred)
+  return tree 
+
+def get_parent_child_sets(tree):
+  parent_child_relationships = tree.pred;
+  cleaned_parent_child_relationships = []
+  for node in parent_child_relationships:
+    cur_preds = list(parent_child_relationships[node])
+    if cur_preds:
+      cleaned_parent_child_relationships.append((node, cur_preds[0]))
+  return cleaned_parent_child_relationships 
+    
+def calculate_parent_child_distance(tree1, tree2): 
+  tree1_set = set(get_parent_child_sets(tree1))
+  tree2_set = set(get_parent_child_sets(tree2)) 
+  symmetric_difference = tree1_set ^ tree2_set
+  print(symmetric_difference)
+  return len(symmetric_difference)
+   
+   
+tree1_filename = "../examples/trees/tree1.dot" 
+tree2_filename = "../examples/trees/tree2.dot" 
+tree1 = read_dot_file(tree1_filename)
+tree2 = read_dot_file(tree2_filename)
+print(calculate_parent_child_distance(tree1, tree2))
