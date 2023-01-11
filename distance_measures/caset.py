e8d7d7d6cc7d3d6a118f7fb2b28bc9e17a30fce8
quocodile <nguyenq2@carleton.edu>
2023-01-10 20:36:25 -0600

    fixing our caset code

--- distance_measures/caset.py
+++ distance_measures/caset.py
@@ -45,16 +45,22 @@ def get_contributions(g_1, g_2):
             caset_2 = get_common_ancestor_set(mut_1, mut_2, mutation_anc_dict_2)
             caset_union = caset_1.union(caset_2)
             x = len(caset_union)
+            caset_intersection = caset_1.intersection(caset_2)
+            y = len(caset_intersection)
             # print(caset_union)
+            jacc_dist = (x - y) / x
             caset_set_minus_1 = caset_1.difference(caset_2)
             caset_set_minus_2 = caset_2.difference(caset_1)
             for set_minus_1_mut in caset_set_minus_1:
-                dict_1[get_node_from_mutation(g_1, set_minus_1_mut)]["contribution"] += 1/(2*x)
-                caset_distance += 1/(2*x)
+                dict_1[get_node_from_mutation(g_1, set_minus_1_mut)]["contribution"] += jacc_dist / len(caset_set_minus_1) / 2
+                caset_distance += jacc_dist / len(caset_set_minus_1) / 2 
             for set_minus_2_mut in caset_set_minus_2:                
-                dict_2[get_node_from_mutation(g_2, set_minus_2_mut)]["contribution"] += 1/(2*x)
-                caset_distance += 1/(2*x)
+                dict_2[get_node_from_mutation(g_2, set_minus_2_mut)]["contribution"] += jacc_dist / len(caset_set_minus_2) / 2
+                caset_distance +=  jacc_dist / len(caset_set_minus_2) / 2
     # return caset_distance, dict_1, dict_2
+    m = len(full_mutation_set)
+    dist = (1/(m*((m-1)/2)) * caset_distance) # m choose 2
+    print(dist)
     return dict_1, dict_2
 
 # def jacc(mutation_1, mutation_2, mutation_anc_dict_1, mutation_anc_dict_2):  
@@ -97,6 +103,7 @@ def get_root(g):
     ''' Returns node with in-degree 0. Exits and
         prints error if multiple such nodes exist '''
     root_candidates = set(g.nodes)
+    root_candidates = set([x for x in root_candidates if "\\" not in x])
     all_nodes = g.nodes
     for a in all_nodes:
         for b in g.successors(a):
