fc0f5ef1404adddbc42d9192a0e05d340ad86490
JuntaoZhong <zhongj2@carleton.edu>
2023-02-05 13:58:41 -0600

    dot to newick conversion working

--- api.py
+++ api.py
@@ -14,14 +14,13 @@ import distance_measures.parent_child as pc_dot
 import distance_measures.ancestor_descendant as ad_dot
 import distance_measures.caset as cs_dot
 import distance_measures.disc as disc_dot
-import input_conversion.Newick_2_dot as Newick_2_dot
+import input_conversion.Newick_2_dot_2 as Newick_2_dot
 
 api = flask.Blueprint('api', __name__)
 
 def write_dot_tree_2_file(dot_tree_str, filename):
-  temp_file = open(filename, "w")
-  temp_file.write(dot_tree_str)
-  temp_file.close()
+  with open(filename, "w") as f:
+    f.write(dot_tree_str)
 
 @api.route('/showTreeFromDot/<dot_fname>')
 def view_tree(dot_fname):
@@ -57,33 +56,33 @@ def run_parent_child_distance():
   tree1_type = flask.request.args.get('treeType1')
   tree2_type = flask.request.args.get('treeType2')
 
-  dot_tree1_file, dot_tree2_file = "t1.txt", "t2.txt" # dot formatted trees.
-
-  if tree1_type == "Newick":
+  if tree1_type == "newick":
     tree1_data = Newick_2_dot.convert_newick_2_dot(tree1_data)
-  # elif tree1_type == "DOT": // don't do anything to tree1_data
-
-  if tree2_type == "Newick":
-    tree2_data = Newick_2_dot.convert_newick_2_dot(tree1_data)
+  if tree2_type == "newick":
+    tree2_data = Newick_2_dot.convert_newick_2_dot(tree2_data)
   
-  write_dot_tree_2_file(tree1_data, dot_tree1_file)
-  write_dot_tree_2_file(tree2_data, dot_tree2_file)
+  write_dot_tree_2_file(tree1_data, "t1.txt")
+  write_dot_tree_2_file(tree2_data, "t2.txt")
 
-  data_1, data_2, distance = pc_dot.pc_main(dot_tree1_file, dot_tree2_file)
+  data_1, data_2, distance = pc_dot.pc_main("t1.txt", "t2.txt")
   jsonObject = {"tree1_edges": data_1, "tree2_edges": data_2, "distance": distance}
   print(json.dumps(jsonObject))
   return(json.dumps(jsonObject))
   
 @api.route('/ancestor_descendant_distance')
 def run_ancestor_descendant_distance():
-  tree1_dot = flask.request.args.get('tree1') 
-  tree2_dot = flask.request.args.get('tree2') 
-  temp_t1 = open("t1.txt", "w")
-  temp_t2 = open("t2.txt", "w")
-  temp_t1.write(tree1_dot)
-  temp_t2.write(tree2_dot)
-  temp_t1.close()
-  temp_t2.close()
+  tree1_data = flask.request.args.get('tree1')
+  tree2_data = flask.request.args.get('tree2')
+  tree1_type = flask.request.args.get('treeType1')
+  tree2_type = flask.request.args.get('treeType2')
+
+  if tree1_type == "newick":
+    tree1_data = Newick_2_dot.convert_newick_2_dot(tree1_data)
+  if tree2_type == "newick":
+    tree2_data = Newick_2_dot.convert_newick_2_dot(tree2_data)
+  
+  write_dot_tree_2_file(tree1_data, "t1.txt")
+  write_dot_tree_2_file(tree2_data, "t2.txt")
   data_1, data_2, distance = ad_dot.ad_main("t1.txt", "t2.txt")
   jsonObject = {"tree1_edges": data_1, "tree2_edges": data_2, "distance": distance}
   print(json.dumps(jsonObject))
@@ -91,14 +90,18 @@ def run_ancestor_descendant_distance():
 
 @api.route('/caset_distance')
 def run_caset_distance():
-  tree1_dot = flask.request.args.get('tree1') 
-  tree2_dot = flask.request.args.get('tree2') 
-  temp_t1 = open("t1.txt", "w")
-  temp_t2 = open("t2.txt", "w")
-  temp_t1.write(tree1_dot)
-  temp_t2.write(tree2_dot)
-  temp_t1.close()
-  temp_t2.close()
+  tree1_data = flask.request.args.get('tree1')
+  tree2_data = flask.request.args.get('tree2')
+  tree1_type = flask.request.args.get('treeType1')
+  tree2_type = flask.request.args.get('treeType2')
+
+  if tree1_type == "newick":
+    tree1_data = Newick_2_dot.convert_newick_2_dot(tree1_data)
+  if tree2_type == "newick":
+    tree2_data = Newick_2_dot.convert_newick_2_dot(tree2_data)
+  
+  write_dot_tree_2_file(tree1_data, "t1.txt")
+  write_dot_tree_2_file(tree2_data, "t2.txt")
   data_1, data_2, distance = cs_dot.cs_main("t1.txt", "t2.txt")
   jsonObject = {"tree1_edges": data_1, "tree2_edges": data_2, "distance": distance}
   print(json.dumps(jsonObject))
@@ -106,14 +109,18 @@ def run_caset_distance():
 
 @api.route('/disc_distance')
 def run_disc_distance():
-  tree1_dot = flask.request.args.get('tree1') 
-  tree2_dot = flask.request.args.get('tree2') 
-  temp_t1 = open("t1.txt", "w")
-  temp_t2 = open("t2.txt", "w")
-  temp_t1.write(tree1_dot)
-  temp_t2.write(tree2_dot)
-  temp_t1.close()
-  temp_t2.close()
+  tree1_data = flask.request.args.get('tree1')
+  tree2_data = flask.request.args.get('tree2')
+  tree1_type = flask.request.args.get('treeType1')
+  tree2_type = flask.request.args.get('treeType2')
+
+  if tree1_type == "newick":
+    tree1_data = Newick_2_dot.convert_newick_2_dot(tree1_data)
+  if tree2_type == "newick":
+    tree2_data = Newick_2_dot.convert_newick_2_dot(tree2_data)
+  
+  write_dot_tree_2_file(tree1_data, "t1.txt")
+  write_dot_tree_2_file(tree2_data, "t2.txt")
   data_1, data_2, distance = disc_dot.disc_main("t1.txt", "t2.txt")
   jsonObject = {"tree1_edges": data_1, "tree2_edges": data_2, "distance": distance}
   print(json.dumps(jsonObject))
